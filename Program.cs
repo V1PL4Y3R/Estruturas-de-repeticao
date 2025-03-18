ae99826380710ffd7e1b82c62cde57d26d23e98d
FIC <fic@info.local>
2025-03-17 21:56:40 -0300

    Exercicio_estruturas de repeticao

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+
+
+
+//for (int i = 1; i < 21; i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+int senha;
+
+do
+{
+    Console.WriteLine("Informe a senha");
+ senha = int.Parse(Console.ReadLine());
+} while (senha == 1234); 
+
+
+ 
